42d1651ddc30b7bc0373dde574439485bb5846d3
durgasankar <durgasankar.raja500@gmail.com>
2023-01-18 00:23:47 +0530

    added router module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageNotFound from './common/PageNotFound';
 
 function App() {
   return (
     <div >
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/404' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
