dc951e3353e1131dd47e68678c6387c9af90e2b9
durgasankar <durgasankar.raja500@gmail.com>
2023-01-18 00:19:41 +0530

    page not found designing

---
+++ src/common/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <section className='not-found'>
+            <h1 className='not-found--title heading-primary'>Oops! Page Not Found</h1>
+            <h2 className='not-found--subtitle heading-secondary'>¯\_(ツ)_/¯</h2>
+        </section>
+    )
+}
+
+export default PageNotFound;
