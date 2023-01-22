8b6868b4a743b64a931ccc2627cffe7ea67d5a69
durgasankar <durgasankar.raja500@gmail.com>
2023-01-22 10:39:28 +0530

    addition of loading spinner for modal and handle case quotes api failure

--- src/common/Spinners.jsx
+++ src/common/Spinners.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { MetroSpinner } from "react-spinners-kit";
+import { Segment, Loader, Dimmer, Image } from 'semantic-ui-react';
+import blur_image from '../assets/images/short_aparagraph.png';
 
+/**
+ * Whole page loading spinner
+ * @returns MetroSpinner
+ */
 export const PageLoadingSpinner = () => {
     const isLoading = true;
     return (
@@ -14,3 +20,19 @@ export const PageLoadingSpinner = () => {
         </div>
     )
 };
+
+/**
+ * Symantic UI Blury image spinner
+ * @returns BlurtImage Spinner
+ */
+export const BlurImageLoadingSpinner = () => {
+    return (
+        <Segment >
+            <Dimmer active inverted>
+                <Loader indeterminate inverted
+                    content={<h4 className='paragraph'>Loading...Please wait!</h4>} />
+            </Dimmer>
+            <Image src={blur_image} />
+        </Segment>
+    )
+}
--- src/components/Stocks/QuotesModal.jsx
+++ src/components/Stocks/QuotesModal.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { Modal } from 'semantic-ui-react';
 import { CrossButton } from '../../common/CustomButton';
 import { history } from '../../utils/history';
+import API from '../../utils/API';
+import { GetRequest } from '../../utils/HttpRequestHandler';
+import { BlurImageLoadingSpinner } from '../../common/Spinners';
 
 const QuotesModal = (props) => {
     const { symbol } = useParams();
     const { name, validTill } = useLocation()?.state;
     const { isQuotesModalOpen, setQuotesModalOpen } = props;
 
+    const [isQuotesDataLoading, seQuotesDataLoading] = useState(true);
+    const [isQuotesFetchingFailed, setQuotesFetchingFailed] = useState(false);
+
+    const _isQuotesLoading = isLoading => seQuotesDataLoading(isLoading);
+
+    const getQuotesOfStockAPI = async () => {
+        try {
+            await GetRequest(`${API.QUOTES}/${symbol}`)
+                .then(async response => {
+                    if (response?.status === 200) {
+
+                    }
+                }).catch(async error => error && setQuotesFetchingFailed(true))
+            await _isQuotesLoading(false);
+        } catch (error) {
+            _isQuotesLoading(false);
+            history.push('/404');
+        }
+    }
+
     const handleCloseModal = () => {
         setQuotesModalOpen(false);
         history.push('/stocks');
     }
+
+    useEffect(() => {
+        getQuotesOfStockAPI();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
         <>
             <Modal
@@ -22,18 +50,30 @@ const QuotesModal = (props) => {
                 closeOnEscape={handleCloseModal}
             >
                 <Modal.Header>
-                    <div className="modal-header">
-                        <div className="modal-header__text">
+                    <div className="modal__header">
+                        <div className="modal__header--text">
                             {`[${symbol}] : ${name}`}
                         </div>
-                        <div className="modal-header__close">
+                        <div className="modal__header--close">
                             <CrossButton onClick={handleCloseModal} />
                         </div>
                     </div>
                 </Modal.Header>
-                <Modal.Content image scrolling>
-                    <Modal.Description>
-                    </Modal.Description>
+                <Modal.Content scrolling>
+                    <div className="modal__body">
+                        <div className="modal__body__operations">
+
+                        </div>
+                        <div className="modal__body__table">
+
+                        </div>
+                        {
+                            isQuotesDataLoading && <BlurImageLoadingSpinner />
+                        }
+                        {
+                            isQuotesFetchingFailed && <span className='modal__body--failed-to-load paragraph '>Oops! Failed to load. Please refresh.</span>
+                        }
+                    </div>
                 </Modal.Content>
             </Modal>
         </>
